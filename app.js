2aed2920080ec2833fa12c042d4197903395cd37
paul.wu <tease0623@gmail.com>
2023-04-27 16:41:10 +0800

    search restaurants with name or category

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ const restaurants = restaurantList.results
 const app = express()
 const port = 3000
 
-app.engine('hbs', hbs.engine({defaultLayout: 'main', extname: 'hbs'}))
+app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', {restaurants})
+    res.render('index', { restaurants })
 })
 
 app.get('/restaurants/:id', (req, res) => {
     const id = Number(req.params.id)
     const restaurant = restaurants.find(restaurant => restaurant.id === id)
-    res.render('show', {restaurant})
+    res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurantsFilter = restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) 
+        || restaurant.category.toLocaleLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('index', { restaurants: restaurantsFilter })
 })
 
 app.listen(port, () => {
